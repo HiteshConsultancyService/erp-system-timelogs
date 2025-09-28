feba66c456a291273f203791e6923b2342ba3bec
hitesh-ahirwar <ahirwarhitesh1902@gmail.com>
2025-09-28 18:41:53 +0530

    Initial commit

---
+++ src/test/java/com/erpsystem/timelogs/TimelogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erpsystem.timelogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
